68befe4691dda7244d5d3689619c7953cd17dd82
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-05 15:13:10 +0100

    task05: init

---
+++ htotbagi/day05/src/Task05.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Text;
+
+namespace Day05Src
+{
+    public class Alamac
+    {
+        public List<int> GetAllSeeds(string filePath)
+        {
+            string seedsLine = File.ReadAllText(filePath).Split('\n').FirstOrDefault(line => line.Contains("seeds:"));
+            return ExtractNumbers(seedsLine);
+        }
+
+        public List<int> ExtractNumbers(string input)
+        {
+            return input?.Split(' ')
+                         .Where(word => int.TryParse(word, out _))
+                         .Select(int.Parse)
+                         .ToList() ?? new List<int>();
+        }
+
+
+        public List<string> ReadFileToList(string filePath)
+        {
+            List<string> linesList = new List<string>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                linesList.AddRange(lines);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            return linesList;
+        }
+
+        public List<List<int>> GetAllMappings(string filePath, string mappingName)
+        {
+            List<List<int>> result = new List<List<int>>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            int mappingIndex = Array.IndexOf(lines, mappingName);
+
+            for (int i = mappingIndex + 1; i < lines.Length && !string.IsNullOrWhiteSpace(lines[i]); i++)
+            {
+                List<int> mappingValues = lines[i].Split(' ').Select(int.Parse).ToList();
+                result.Add(mappingValues);
+            }
+
+            return result;
+        }
+
+        public List<int> CreateEntireMapping(List<List<int>> mappings)
+        {
+            List<int> result = Enumerable.Range(0, 108).ToList();
+
+            foreach (var mapping in mappings)
+            {
+                int destinationRange = mapping[0];
+                int sourceRange = mapping[1];
+                int rangeLength = mapping[2];
+
+                for (int j = sourceRange; j < sourceRange + rangeLength; j++)
+                {
+                    result[j] = destinationRange++;
+                }
+            }
+
+            return result;
+        }
+
+
+        public int GetSeedLoaction(int seedNumber, string filePath)
+        {
+            List<int> seedToSoilMap = CreateMap(filePath, "seed-to-soil map:");
+            List<int> soilToFertilizerMap = CreateMap(filePath, "soil-to-fertilizer map:");
+            List<int> fertilizerToWaterMap = CreateMap(filePath, "fertilizer-to-water map:");
+            List<int> waterToLightMap = CreateMap(filePath, "water-to-light map:");
+            List<int> lightToTemperatureMap = CreateMap(filePath, "light-to-temperature map:");
+            List<int> temperatureToHumidityMap = CreateMap(filePath, "temperature-to-humidity map:");
+            List<int> humidityToLocationMap = CreateMap(filePath, "humidity-to-location map:");
+
+            int seedToSoil = GetValueFromMapping(seedToSoilMap, seedNumber);
+            int soilToFertilizer = GetValueFromMapping(soilToFertilizerMap, seedToSoil);
+            int fertilizerToWater = GetValueFromMapping(fertilizerToWaterMap, soilToFertilizer);
+            int waterToLight = GetValueFromMapping(waterToLightMap, fertilizerToWater);
+            int lightToTemperature = GetValueFromMapping(lightToTemperatureMap, waterToLight);
+            int temperatureToHumidity = GetValueFromMapping(temperatureToHumidityMap, lightToTemperature);
+            int humidityToLocation = GetValueFromMapping(humidityToLocationMap, temperatureToHumidity);
+
+            return humidityToLocation;
+        }
+
+        private List<int> CreateMap(string filePath, string mappingKey)
+        {
+            return CreateEntireMapping(GetAllMappings(filePath, mappingKey));
+        }
+
+        private int GetValueFromMapping(List<int> mapping, int index)
+        {
+            return mapping[index];
+        }
+
+
+        public int GetLowestLocationNumber(string filePath)
+        {
+            List<int> allSeeds = GetAllSeeds(filePath);
+
+            List<int> result = new List<int>();
+
+            foreach (int seedNumber in allSeeds)
+            {
+                var res = GetSeedLoaction(seedNumber, filePath);
+                result.Add(res);
+            }
+
+            return result.Min();
+        }
+    }
+}
---
+++ htotbagi/day05/test/Task05Test.cs
@@ -0,0 +1,102 @@
+﻿using Day05Src;
+using FluentAssertions;
+using Xunit;
+
+namespace Day05Tests
+{
+    public class Task05Test
+    {
+        public string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day05Src\\data\\alamac.txt";
+        Alamac newAlamac = CreateAlamac();
+
+        [Fact]
+        public void Should_return_all_seeds()
+        {
+            // Arrange
+            List<int> expected = new List<int> { 79, 14, 55, 13 };
+
+            // Act
+            List<int> result = newAlamac.GetAllSeeds(filePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { "seed-to-soil map:", new List<List<int>> { new List<int> { 50, 98, 2 }, new List<int> { 52, 50, 48 } } };
+            yield return new object[] { "soil-to-fertilizer map:", new List<List<int>> { new List<int> { 0, 15, 37 }, new List<int> { 37, 52, 2 }, new List<int> { 39, 0, 15 } } };
+            yield return new object[] { "humidity-to-location map:", new List<List<int>> { new List<int> { 60, 56, 37 }, new List<int> { 56, 93, 4 } } };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void Should_return_list_of_all_mappings_based_on_mapping_name(string mappingName, List<List<int>> expectedMappings)
+        {
+            // Act
+            List<List<int>> result = newAlamac.GetAllMappings(filePath, mappingName);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedMappings);
+        }
+
+        [Fact]
+        public void Should_create_entire_mapping()
+        {
+            // Arrange
+            List<List<int>> mappings = new List<List<int>>
+            {
+                new List<int> { 50, 98, 2 },
+                new List<int> { 52, 50, 48 }
+            };
+
+            List<int> expected_seed = new List<int> {0, 1,2,3,4,5,6,7,8,9,10,
+                                                    11,12,13,14,15,16,17,18,19,20,
+                                                    21,22,23,24,25,26,27,28,29,30,
+                                                    31,32,33,34,35,36,37,38,39,40,
+                                                    41,42,43,44,45,46,47,48,49,52,
+                                                    53,54,55,56,57,58,59,60,61,62,
+                                                    63,64,65,66,67,68,69,70,71,72,
+                                                    73,74,75,76,77,78,79,80,81,82,
+                                                    83,84,85,86,87,88,89,90,91,92,
+                                                    93,94,95,96,97,98,99,50,51,100,
+                                                    101,102,103,104,105,106,107};
+
+            List<int> result = newAlamac.CreateEntireMapping(mappings);
+
+            // Assert
+            result.Should().Equal(expected_seed);
+        }
+
+        [Theory]
+        [InlineData(79, 82)]
+        [InlineData(14, 43)]
+        [InlineData(55, 86)]
+        [InlineData(13, 35)]
+        public void Should_return_location_for_seeds(int seedNumber, int expected)
+        {
+            // Act
+            int result = newAlamac.GetSeedLoaction(seedNumber, filePath);
+
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_return_lowest_location_number()
+        {
+            // Arrange
+            int expected = 35;
+
+            // Act
+            int result = newAlamac.GetLowestLocationNumber(filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Alamac CreateAlamac()
+        {
+            return new Alamac();
+        }
+    }
+}
