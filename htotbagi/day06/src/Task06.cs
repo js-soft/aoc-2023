4a3f7939589b7703f6323c20d6f65d305a5ac193
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-06 07:12:08 +0100

    task06: init

---
+++ htotbagi/day06/src/Task06.cs
@@ -0,0 +1,81 @@
+﻿namespace Day06Src
+{
+    public class Race
+    {
+        public List<int> GetTimes(string filePath)
+        {
+            string startsWith = "Time:";
+            List<int> timeValues = ExtractTimeValues(filePath, startsWith);
+            return timeValues;
+        }
+
+        public List<int> GetDistances(string filePath)
+        {
+            string startsWith = "Distance:";
+            List<int> timeValues = ExtractTimeValues(filePath, startsWith);
+            return timeValues;
+        }
+
+        public List<int> ExtractTimeValues(string filePath, string startsWith)
+        {
+            List<int> timeValues = new List<int>();
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            string timeLine = lines.FirstOrDefault(line => line.StartsWith(startsWith));
+
+            if (timeLine != null)
+            {
+                string[] words = timeLine.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 1; i < words.Length; i++)
+                {
+                    if (int.TryParse(words[i], out int timeValue))
+                    {
+                        timeValues.Add(timeValue);
+                    }
+
+                }
+            }
+
+            return timeValues;
+        }
+
+        public int CalculateDifferentWaysForSpecificRace(string filePath, int raceNumber)
+        {
+            List<int> times = GetTimes(filePath);
+            List<int> distances = GetDistances(filePath);
+
+            List<int> result = new List<int>();
+
+            int firstTime = times[raceNumber];
+            int counter = times[raceNumber];
+            int firstDistance = distances[raceNumber];
+
+            for (int i = 0; i < firstTime; i++)
+            {
+                if (i * counter > firstDistance)
+                {
+                    result.Add(i * counter);
+                }
+                counter -= 1;
+            }
+
+            return result.Count();
+        }
+
+        public int GetMarginError(string filePath)
+        {
+            List<int> times = GetTimes(filePath);
+
+            int result = 1;
+
+            for (int i = 0; i < times.Count; i++)
+            {
+                result *= CalculateDifferentWaysForSpecificRace(filePath, i);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ htotbagi/day06/test/Task06Test.cs
@@ -0,0 +1,68 @@
+﻿using Day06Src;
+using FluentAssertions;
+using Xunit;
+
+namespace Day06Tests
+{
+    public class Task06Test
+    {
+        Race newRace = CreateRace();
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\exampleRace.txt", new[] { 7, 15, 30 })]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\realRace.txt", new[] { 48, 87, 69, 81 })]
+        public void Should_get_times(string filePath, int[] expected)
+        {
+            // Act
+            List<int> result = newRace.GetTimes(filePath);
+
+            // Assert
+            result.Should().Equal(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\exampleRace.txt", new[] { 9, 40, 200 })]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\realRace.txt", new[] { 255, 1288, 1117, 1623 })]
+        public void Should_get_distances(string filePath, int[] expected)
+        {
+            // Act
+            List<int> result = newRace.GetDistances(filePath);
+
+            // Assert
+            result.Should().Equal(expected);
+        }
+
+        [Theory]
+        [InlineData(0, 4)]
+        [InlineData(1, 8)]
+        [InlineData(2, 9)]
+        public void Should_return_number_of_different_ways(int raceNumber, int expected)
+        {
+            // Arrange
+            string filePath = "C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\exampleRace.txt";
+
+            // Act
+            int result = newRace.CalculateDifferentWaysForSpecificRace(filePath, raceNumber);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\exampleRace.txt", 288)]
+        [InlineData("C:\\Users\\htotbagi\\OneDrive - j&s-soft GmbH\\Dokumente\\C#-10-fundamentals\\advent-of-code\\Day06Src\\data\\realRace.txt", 252000)]
+        public void Should_return_margin_of_error(string filePath, int expected)
+        {
+            // Act
+            int result = newRace.GetMarginError(filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Race CreateRace()
+        {
+            return new Race();
+        }
+    }
+}
