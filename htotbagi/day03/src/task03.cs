d69305864d267002ea330990a26dd01c5bf35af2
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-08 18:50:06 +0100

    task03: init

---
+++ htotbagi/day03/src/task03.cs
@@ -0,0 +1,167 @@
+﻿namespace test.day03_gear_ratios
+{
+    public class Schematic
+    {
+        public List<string> ExtendSchematicWithDots(string filePath)
+        {
+            List<string> matrix = ReadFileToList(filePath);
+
+            int rows = matrix.Count;
+            int cols = matrix[0].Length;
+
+            List<string> extendedMatrix = new List<string>(rows + 2);
+
+            extendedMatrix.Add(new string('.', cols + 2));
+
+            for (int i = 0; i < rows; i++)
+            {
+                extendedMatrix.Add("." + matrix[i] + ".");
+            }
+
+            extendedMatrix.Add(new string('.', cols + 2));
+
+            return extendedMatrix;
+        }
+
+        public bool IsTheNumberFirst(List<string> extendedSchema, int i, int j)
+        {
+            if (extendedSchema[i - 1][j] == '.' &&
+                extendedSchema[i - 1][j - 1] == '.' &&
+                extendedSchema[i][j - 1] == '.' &&
+                extendedSchema[i + 1][j - 1] == '.' &&
+                extendedSchema[i + 1][j] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTheNumberMiddle(List<string> extendedSchema, int i, int j)
+        {
+            if (extendedSchema[i - 1][j] == '.' && extendedSchema[i + 1][j] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsTheNumberLast(List<string> extendedSchema, int i, int j)
+        {
+            if (extendedSchema[i - 1][j] == '.' &&
+                extendedSchema[i - 1][j + 1] == '.' &&
+                extendedSchema[i][j + 1] == '.' &&
+                extendedSchema[i + 1][j + 1] == '.' &&
+                extendedSchema[i + 1][j] == '.')
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Dimension1(string filePath, int i, int j)
+        {
+            List<string> matrix = ExtendSchematicWithDots(filePath);
+
+            if (IsTheNumberFirst(matrix, i, j) && IsTheNumberLast(matrix, i, j))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Dimension2(string filePath, int i, int j)
+        {
+            List<string> matrix = ExtendSchematicWithDots(filePath);
+
+            if (IsTheNumberFirst(matrix, i, j - 1) && IsTheNumberLast(matrix, i, j))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Dimension3(string filePath, int i, int j)
+        {
+            List<string> matrix = ExtendSchematicWithDots(filePath);
+
+            if (IsTheNumberFirst(matrix, i, j - 2) && IsTheNumberMiddle(matrix, i, j - 1) && IsTheNumberLast(matrix, i, j))
+            {
+                return true;
+            }
+            return false; ;
+        }
+
+        public int GetFinalResult(string filePath)
+        {
+            List<string> linesList = ExtendSchematicWithDots(filePath);
+
+            int row = linesList.Count();
+            int col = linesList[0].Length;
+            int totalSum = 0;
+            int counter = 0;
+
+            for (int i = 1; i < row - 1; i++)
+            {
+                for (int j = 1; j < col - 1; j++)
+                {
+                    if (char.IsDigit(linesList[i][j]))
+                    {
+                        counter++;
+
+                        if (!char.IsDigit(linesList[i][j + 1]))
+                        {
+                            if (counter == 1 && !Dimension1(filePath, i, j))
+                            {
+                                string combination = linesList[i][j].ToString();
+                                totalSum += int.Parse(combination);
+                            }
+
+                            if (counter == 2 && !Dimension2(filePath, i, j))
+                            {
+                                string combination = ConvertToDoubleNumber(linesList, i, j);
+                                totalSum += int.Parse(combination);
+                            }
+
+                            if (counter == 3 && !Dimension3(filePath, i, j))
+                            {
+                                string combination = ConvertToTripleNumber(linesList, i, j);
+                                totalSum += int.Parse(combination);
+                            }
+                            counter = 0;
+                        }
+                    }
+                }
+            }
+            return totalSum;
+        }
+
+        private static string ConvertToDoubleNumber(List<string> linesList, int i, int j)
+        {
+            string lastChar = linesList[i][j].ToString();
+            string secondLastChar = linesList[i][j - 1].ToString();
+
+            string combination = secondLastChar + lastChar;
+            return combination;
+        }
+
+        private static string ConvertToTripleNumber(List<string> linesList, int i, int j)
+        {
+            string lastChar = linesList[i][j].ToString();
+            string secondLastChar = linesList[i][j - 1].ToString();
+            string thirdLastChar = linesList[i][j - 2].ToString();
+
+            string combination = thirdLastChar + secondLastChar + lastChar;
+            return combination;
+        }
+
+        public List<string> ReadFileToList(string filePath)
+        {
+            List<string> linesList = new List<string>();
+
+            string[] lines = File.ReadAllLines(filePath);
+            linesList.AddRange(lines);
+
+            return linesList;
+        }
+    }
+}
---
+++ htotbagi/day03/test/task03test.cs
@@ -0,0 +1,136 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace test.day03_gear_ratios
+{
+    public class task03test
+    {
+        Schematic newSchematic = CreateSchematic();
+
+        [Theory]
+        [InlineData("singleDigit.txt", "singleDigitExtended.txt")]
+        [InlineData("doubleDigit.txt", "doubleDigitExtended.txt")]
+        [InlineData("trippleDigit.txt", "trippleDigitExtended.txt")]
+        public void Should_extend_given_file_with_dots(string firstFileName, string secondFileName)
+        {
+            // Arrange
+            var firstFilePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + firstFileName;
+            var secondFilePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + secondFileName;
+
+            var expected = newSchematic.ReadFileToList(secondFilePath);
+
+            // Act
+            List<string> result = newSchematic.ExtendSchematicWithDots(firstFilePath);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("trippleDigitExtended.txt", 1, 1)]
+        public void Is_the_number_first(string fileName, int rowIndex, int colIndex)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+            var extendedSchema = newSchematic.ReadFileToList(filePath);
+
+            // Act
+            bool result = newSchematic.IsTheNumberFirst(extendedSchema, rowIndex, colIndex);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("trippleDigitExtended.txt", 1, 2)]
+        public void Is_the_number_middle(string fileName, int rowIndex, int colIndex)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+            var extendedSchema = newSchematic.ReadFileToList(filePath);
+
+            // Act
+            bool result = newSchematic.IsTheNumberMiddle(extendedSchema, rowIndex, colIndex);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("trippleDigitExtended.txt", 1, 3)]
+        public void Is_the_number_last(string fileName, int rowIndex, int colIndex)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+            var extendedSchema = newSchematic.ReadFileToList(filePath);
+
+            // Act
+            bool result = newSchematic.IsTheNumberLast(extendedSchema, rowIndex, colIndex);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("singleDigit.txt", 1, 1)]
+        public void single_digit_all_points_around(string fileName, int rowIndex, int columnIndex)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+
+            // Act
+            bool result = newSchematic.Dimension1(filePath, rowIndex, columnIndex);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("doubleDigit.txt", 1, 2)]
+        public void double_digit_all_points_around(string fileName, int rowIndex, int columnIndex)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+
+            // Act
+            bool result = newSchematic.Dimension2(filePath, rowIndex, columnIndex);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("trippleDigit.txt", 1, 3)]
+        public void tripple_digit_all_points_around(string fileName, int rowIndex, int columnIndex)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+
+            // Act
+            bool result = newSchematic.Dimension3(filePath, rowIndex, columnIndex);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("engineSchematic.txt", 4361)]
+        [InlineData("realSchematics.txt", 544433)]
+        public void Should_return_correct_sum(string fileName, int expected)
+        {
+            // Arrange
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../aoc/day03-gear-ratios/data/" + fileName;
+
+            // Act
+            int result = newSchematic.GetFinalResult(filePath);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Schematic CreateSchematic()
+        {
+            return new Schematic();
+        }
+    }
+}
