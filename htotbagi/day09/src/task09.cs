4fe5ba876a7fcec50f2f2b645554d3829ea3b929
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-09 11:26:38 +0100

    task09: init

---
+++ htotbagi/day09/src/task09.cs
@@ -0,0 +1,74 @@
+﻿namespace src.day09
+{
+    public class Sensor
+    {
+        public List<int> GetNextRowCalculation(List<int> stringRow)
+        {
+            return stringRow.Zip(stringRow.Skip(1), (current, next) => next - current).ToList();
+        }
+
+        public List<List<int>> GetAllDifferencesForThatRow(List<int> row)
+        {
+            List<List<int>> storage = new List<List<int>> { row };
+
+            while (true)
+            {
+                List<int> nextRow = GetNextRowCalculation(storage[^1]);
+                storage.Add(nextRow);
+
+                if (nextRow.All(value => value == 0))
+                {
+                    break;
+                }
+            }
+
+            return storage;
+        }
+
+        public int AddStepsToAsALastElement(List<List<int>> input)
+        {
+            input[^1].Add(0);
+
+            for (int i = input.Count - 1; i >= 1; i--)
+            {
+                int lastElement = input[i].Last();
+                input[i - 1].Add(input[i - 1].Last() + lastElement);
+            }
+
+            return input[0][^1];
+        }
+
+
+        public int CalcFinal(string filePath)
+        {
+            List<List<int>> data = newExtractor(filePath);
+
+            int result = 0;
+
+            foreach (List<int> row in data)
+            {
+                result += AddStepsToAsALastElement(GetAllDifferencesForThatRow(row));
+            }
+
+            return result;
+        }
+
+
+        public List<List<int>> newExtractor(string filePath)
+        {
+            List<List<int>> result = ReadFile(filePath);
+
+            return result;
+        }
+
+        static List<List<int>> ReadFile(string filePath)
+        {
+            return File.ReadAllLines(filePath)
+                       .Select(line => line.Split(' ')
+                                            .Select(value => int.TryParse(value, out int intValue) ? intValue : 0)
+                                            .ToList())
+                       .ToList();
+        }
+
+    }
+}
---
+++ htotbagi/day09/test/task09test.cs
@@ -0,0 +1,198 @@
+﻿using FluentAssertions;
+using src.day09;
+using Xunit;
+
+namespace test.day9
+{
+    public class task09test
+    {
+        Sensor newSensor = CreateSensor();
+
+        string filePath1 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day09-mirage-maintenance\\data\\exampleFile.txt";
+        string filePath2 = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day09-mirage-maintenance\\data\\realFile.txt";
+
+        [Fact]
+        public void Should_read_in_file()
+        {
+            // Arrange
+            List<List<int>> expected = new List<List<int>>
+            {
+                new List<int> { 0, 3, 6, 9, 12, 15 },
+                new List<int> { 1, 3, 6, 10, 15, 21 },
+                new List<int> { 10, 13, 16, 21, 30, 45 },
+            };
+
+            // Act
+            List<List<int>> result = newSensor.newExtractor(filePath1);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_get_next_row1()
+        {
+            // Arrange
+            List<int> input = new List<int> { 0, 3, 6, 9, 12, 15 };
+            List<int> expected = new List<int> { 3, 3, 3, 3, 3 };
+
+            // Act
+            List<int> result = newSensor.GetNextRowCalculation(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_get_next_row2()
+        {
+            // Arrange
+            List<int> input = new List<int> { 3, 3, 3, 3, 3 };
+            List<int> expected = new List<int> { 0, 0, 0, 0 };
+
+            // Act
+            List<int> result = newSensor.GetNextRowCalculation(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_get_all_differences_for_specific_row0()
+        {
+            // Arrange
+            var input = newSensor.newExtractor(filePath1)[0];
+            List<List<int>> expected = new List<List<int>>
+            {
+                new List<int>{ 0, 3, 6, 9, 12, 15 },
+                new List<int>{ 3, 3, 3, 3, 3 },
+                new List<int>{ 0, 0, 0, 0 },
+            };
+
+            // Act
+            List<List<int>> result = newSensor.GetAllDifferencesForThatRow(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_get_all_differences_for_specific_row1()
+        {
+            // Arrange
+            var input = newSensor.newExtractor(filePath1)[1];
+
+            List<List<int>> expected = new List<List<int>>
+            {
+                new List<int>{ 1, 3, 6, 10, 15, 21 },
+                new List<int>{ 2, 3, 4, 5, 6 },
+                new List<int>{ 1, 1, 1, 1 },
+                new List<int>{ 0, 0, 0 },
+            };
+
+            // Act
+            List<List<int>> result = newSensor.GetAllDifferencesForThatRow(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_get_all_differences_for_specific_row2()
+        {
+            // Arrange
+            var input = newSensor.newExtractor(filePath1)[2];
+
+            List<List<int>> expected = new List<List<int>>
+            {
+                new List<int>{ 10, 13, 16, 21, 30, 45},
+                new List<int>{ 3, 3, 5, 9, 15 },
+                new List<int>{ 0, 2, 4, 6 },
+                new List<int>{ 2, 2, 2 },
+                new List<int>{ 0, 0 },
+            };
+
+            // Act
+            List<List<int>> result = newSensor.GetAllDifferencesForThatRow(input);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Should_add_correct_step0()
+        {
+            // Arrange
+            var firstRow = newSensor.newExtractor(filePath1)[0];
+            List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+            int expected = 18;
+
+            // Act
+            int result = newSensor.AddStepsToAsALastElement(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_add_correct_step1()
+        {
+            // Arrange
+            var firstRow = newSensor.newExtractor(filePath1)[1];
+            List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+            int expected = 28;
+
+            // Act
+            int result = newSensor.AddStepsToAsALastElement(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_add_correct_step2()
+        {
+            // Arrange
+            var firstRow = newSensor.newExtractor(filePath1)[2];
+            List<List<int>> input = newSensor.GetAllDifferencesForThatRow(firstRow);
+            int expected = 68;
+
+            // Act
+            int result = newSensor.AddStepsToAsALastElement(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_return_sum_of_extrapolated_values_for_example_data()
+        {
+            // Arrange
+            int expected = 114;
+
+            // Act
+            int result = newSensor.CalcFinal(filePath1);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_return_sum_of_extrapolated_values_for_real_data()
+        {
+            // Arrange
+            int expected = 1581679977;
+
+            // Act
+            int result = newSensor.CalcFinal(filePath2);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Sensor CreateSensor()
+        {
+            return new Sensor();
+        }
+    }
+}
