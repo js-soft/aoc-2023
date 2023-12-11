400630a2495c02c4800a26940a392656f56199ec
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-11 10:28:55 +0100

    task11: init

---
+++ htotbagi/day11/src/task11.cs
@@ -0,0 +1,101 @@
+﻿namespace src.day11
+{
+    public class Universe
+    {
+        private List<List<char>> galaxy;
+
+        public Universe()
+        {
+            galaxy = ReadFileToGrid("C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day11-cosmic-expansion\\data\\inputExample.txt");
+        }
+
+        public List<List<ulong>> GetCoordinates()
+        {
+            List<List<ulong>> coordinates = new List<List<ulong>>();
+
+            for (int i = 0; i < galaxy.Count; i++)
+            {
+                for (int j = 0; j < galaxy[0].Count; j++)
+                {
+                    if (galaxy[i][j] == '#')
+                    {
+                        coordinates.Add(new List<ulong> { (ulong)i, (ulong)j });
+                    }
+                }
+            }
+            return coordinates;
+        }
+
+        public bool RowIsEmpty(ulong rowIndex)
+        {
+            int rowIndexInt = (int)rowIndex;
+            return galaxy[rowIndexInt].All(cell => cell == '.');
+        }
+
+        public bool ColISEmpty(ulong colIndex)
+        {
+            int colIndexInt = (int)colIndex;
+            return galaxy.All(row => row[colIndexInt] == '.');
+        }
+
+        public ulong GetDistanceBetweenGalaxies(List<ulong> firstPoint, List<ulong> secondPoint, ulong orderOfExpansion)
+        {
+            ulong x1 = firstPoint[0], y1 = firstPoint[1];
+            ulong x2 = secondPoint[0], y2 = secondPoint[1];
+
+            (x1, x2) = (Math.Min(x1, x2), Math.Max(x1, x2));
+            (y1, y2) = (Math.Min(y1, y2), Math.Max(y1, y2));
+
+            ulong result = 0;
+            for (ulong i = x1; i < x2; i++)
+            {
+                result += 1;
+                if (RowIsEmpty(i))
+                {
+                    result += orderOfExpansion - 1;
+                }
+            }
+            for (ulong j = y1; j < y2; j++)
+            {
+                result += 1;
+                if (ColISEmpty(j))
+                {
+                    result += orderOfExpansion - 1;
+                }
+            }
+            return result;
+        }
+
+        public ulong GetTotalDistance(ulong orderOfExpansion)
+        {
+            ulong result = 0;
+
+            List<List<ulong>> coordinates = GetCoordinates();
+
+            for (int i = 0; i < coordinates.Count; i++)
+            {
+                for (int j = i + 1; j < coordinates.Count; j++)
+                {
+                    result = result + GetDistanceBetweenGalaxies(coordinates[i], coordinates[j], orderOfExpansion);
+                }
+            }
+            return result;
+        }
+
+        public List<List<char>> ReadFileToGrid(string filePath)
+        {
+            List<List<char>> grid = new List<List<char>>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<char> row = new List<char>(line.ToCharArray());
+                    grid.Add(row);
+                }
+            }
+            return grid;
+        }
+    }
+}
---
+++ htotbagi/day11/test/task11test.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+using src.day11;
+using Xunit;
+
+namespace test.day11
+{
+    public class task11test
+    {
+        Universe newUniverse = CreateUniverse();
+
+        [Fact]
+        public void Should_get_coordinates()
+        {
+            // Arrange
+            List<List<ulong>> expected = new List<List<ulong>>
+            {
+                new List<ulong> { 0, 3}, new List<ulong> { 1, 7},new List<ulong> { 2, 0},
+                new List<ulong> { 4, 6}, new List<ulong> { 5, 1},new List<ulong> { 6, 9},
+                new List<ulong> { 8, 7}, new List<ulong> { 9, 0},new List<ulong> { 9, 4},
+            };
+
+            // Act
+            List<List<ulong>> result = newUniverse.GetCoordinates();
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(7)]
+        public void Should_return_bool_on_index_row(ulong row)
+        {
+            // Act
+            bool result = newUniverse.RowIsEmpty(row);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(5)]
+        [InlineData(8)]
+        public void Should_return_bool_on_index_column(ulong col)
+        {
+            // Act
+            bool result = newUniverse.ColISEmpty(col);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Should_get_distance_between_galaxies()
+        {
+            // Arrange
+            ulong orderOfExpansion = 2;
+            List<ulong> first = new List<ulong> { 0, 3 };
+            List<ulong> second = new List<ulong> { 1, 7 };
+
+            // Act
+            ulong result = newUniverse.GetDistanceBetweenGalaxies(first, second, orderOfExpansion);
+
+            // Assert
+            result.Should().Be(6);
+        }
+
+        [Theory]
+        [InlineData(2, 374)]
+        [InlineData(10, 1030)]
+        [InlineData(100, 8410)]
+        public void Should_get_total_distance(ulong orderOfExpansion, ulong expected)
+        {
+            // Act
+            ulong result = newUniverse.GetTotalDistance(orderOfExpansion);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        private static Universe CreateUniverse()
+        {
+            return new Universe();
+        }
+    }
+}
