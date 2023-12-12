4fa5d4b674c30641f8ec58e2c3020879717d56d3
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2023-12-12 16:23:27 +0100

    task12: init

---
+++ htotbagi/day12/src/task12.cs
@@ -0,0 +1,145 @@
+﻿namespace src.day12_hot_springs
+{
+    public class HotSpring
+    {
+        public (List<string>, List<List<int>>) ReadFile(string filePath)
+        {
+            var stringsList = new List<string>();
+            var intsList = new List<List<int>>();
+
+            foreach (var line in File.ReadLines(Path.Combine(filePath)))
+            {
+                var parts = line.Split(' ');
+                stringsList.Add(parts[0]);
+                intsList.Add(parts[1].Split(',').Select(int.Parse).ToList());
+            }
+            return (stringsList, intsList);
+        }
+
+        public List<string> SelectStringsWithMatching(string inputString, List<int> numbers)
+        {
+            var result = new List<string>();
+
+            List<string> input = GenerateHotSpringConfigurations(inputString.Length);
+
+            int count = numbers.Sum();
+
+            foreach (string s in input)
+            {
+                if (CountDamagedSprings(s, '#') == count)
+                {
+                    result.Add(s);
+                }
+            }
+            return FindMatchingConfigurations(inputString, result);
+        }
+
+        static List<string> GenerateHotSpringConfigurations(int n)
+        {
+            List<string> result = new List<string>();
+            char[] combination = new char[n];
+            GenerateConfigurationHelper(combination, 0, result);
+            return result;
+        }
+
+        static void GenerateConfigurationHelper(char[] combination, int index, List<string> result)
+        {
+            if (index == combination.Length)
+            {
+                result.Add(new string(combination));
+                return;
+            }
+
+            combination[index] = '.';
+            GenerateConfigurationHelper(combination, index + 1, result);
+
+            combination[index] = '#';
+            GenerateConfigurationHelper(combination, index + 1, result);
+        }
+
+        static int CountDamagedSprings(string input, char target)
+        {
+            return input.Count(c => c == target);
+        }
+
+        static List<string> FindMatchingConfigurations(string pattern, List<string> stringsToCheck)
+        {
+            return stringsToCheck.Where(str => IsMatchingPattern(pattern, str)).ToList();
+        }
+
+        static bool IsMatchingPattern(string pattern, string str)
+        {
+            if (pattern.Length != str.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i] != '?' && pattern[i] != str[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static List<string> FindValidConfigurations(List<string> strings, int[] condition)
+        {
+            List<string> result = new List<string>();
+            foreach (string dobar in strings)
+            {
+                if (IsValidConfiguration(dobar, condition))
+                {
+                    result.Add(dobar);
+                }
+            }
+            return result;
+        }
+
+        private static bool IsValidConfiguration(string dobar, int[] condition)
+        {
+            List<int> lista = new List<int>();
+            int counter = 0;
+
+            foreach (char c in dobar)
+            {
+                if (c == '#')
+                {
+                    counter++;
+                }
+                else if (counter != 0)
+                {
+                    lista.Add(counter);
+                    counter = 0;
+                }
+            }
+
+            if (counter != 0)
+                lista.Add(counter);
+
+            return lista.SequenceEqual(condition);
+        }
+
+        public int CalculateFinalCombinations(string input, int[] numbers)
+        {
+            List<string> help = SelectStringsWithMatching(input, numbers.ToList());
+            List<string> result = FindValidConfigurations(help, numbers);
+
+            return result.Count;
+        }
+
+        public int GetFinalArrangementCount(string filePath)
+        {
+            int result = 0;
+            (List<string> stringResult, List<List<int>> intResult) = ReadFile(filePath);
+
+            for (int i = 0; i < stringResult.Count; i++)
+            {
+                result += CalculateFinalCombinations(stringResult[i], intResult[i].ToArray());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ htotbagi/day12/test/task12test.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using src.day12_hot_springs;
+using Xunit;
+
+namespace Day12_Hot_springs
+{
+    public class RecordTests
+    {
+        string filePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12-hot-springs\\data\\exampleData.txt";
+        string realFilePath = "C:\\Users\\htotbagi\\source\\repos\\aoc\\aoc\\day12-hot-springs\\data\\realData.txt";
+
+        HotSpring newHotSpring = CreateHotSpring();
+
+        [Fact]
+        public void Should_return_symbols_and_numbers_from_input_file()
+        {
+            // Arrange
+            var symbolStrings = new List<string>{ { "???.###" }, { ".??..??...?##." }, { "?#?#?#?#?#?#?#?" },
+                                            { "????.#...#..."}, { "????.######..#####." }, { "?###????????" } };
+
+            var sizeLists = new List<List<int>>{ new List<int>{ 1, 1, 3 }, new List<int>{ 1, 1, 3 }, new List<int>{ 1, 3, 1, 6 },
+                                                new List<int>{ 4, 1, 1 }, new List<int>{ 1, 6, 5 }, new List<int>{ 3, 2, 1 } };
+
+            // Act
+            (List<string> stringResult, List<List<int>> intResult) = newHotSpring.ReadFile(filePath);
+
+            // Assert
+            stringResult.Should().BeEquivalentTo(symbolStrings);
+            intResult.Should().BeEquivalentTo(sizeLists);
+        }
+
+        [Fact]
+        public void Should_return_combinations_that_mach_given_symbols()
+        {
+            // Arrange
+            (List<string> resultStrings, List<List<int>> resultIntegers) = newHotSpring.ReadFile(filePath);
+            List<string> expectedSymbols = new List<string> { ".##.###", "#.#.###", "##..###" };
+
+            // Act
+            List<string> result = newHotSpring.SelectStringsWithMatching(resultStrings[0], resultIntegers[0]);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedSymbols);
+        }
+
+        [Theory]
+        [InlineData("???.###", new int[] { 1, 1, 3 }, 1)]
+        [InlineData(".??..??...?##.", new int[] { 1, 1, 3 }, 4)]
+        [InlineData("?#?#?#?#?#?#?#?", new int[] { 1, 3, 1, 6 }, 1)]
+        [InlineData("????.#...#...", new int[] { 4, 1, 1 }, 1)]
+        [InlineData("????.######..#####.", new int[] { 1, 6, 5 }, 4)]
+        [InlineData("?###????????", new int[] { 3, 2, 1 }, 10)]
+        public void Should_return_number_of_arrangements(string input, int[] numbers, int expected)
+        {
+            int result = newHotSpring.CalculateFinalCombinations(input, numbers);
+
+            result.Should().Be(expected);
+        }
+
+        [Fact]
+        public void Should_return_sum_of_all_arrangements()
+        {
+            // Act
+            int result = newHotSpring.GetFinalArrangementCount(realFilePath);
+
+            // Assert
+            result.Should().Be(7017);
+        }
+
+        private static HotSpring CreateHotSpring()
+        {
+            return new HotSpring();
+        }
+    }
+}
