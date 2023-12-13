45b5310bd4c1050ea9a9d1f2d62b82e5bc091c86
Vladimir Vuckovic <vladimir.vuckovic@js-soft.rs>
2023-12-13 16:07:47 +0100

    chore: added implementation for hours

--- vvuckovi/src/KataTests/HumanReadableTime/human-readable-time.ts
+++ vvuckovi/src/KataTests/HumanReadableTime/human-readable-time.ts
@@ -2,10 +2,11 @@
 /* eslint-disable prettier/prettier */
 export function GetHumanReadableTime(time: number) : string {
     
-    const minutes: number = Math.floor(time / 60);
+    const hours: number = Math.floor(time / 3600);
+    const minutes: number = Math.floor((time % 3600) / 60);
     const seconds: number = time % 60;
 
-    return "00:" + getDoubleDigits(minutes) + ":" + getDoubleDigits(seconds);
+    return getDoubleDigits(hours) + ":" + getDoubleDigits(minutes) + ":" + getDoubleDigits(seconds);
 }
 
 function getDoubleDigits(time: number) : string{
