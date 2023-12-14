55aaa18ab97f9af63c9f7df11841ee352416df65
Vladimir Vuckovic <vladimir.vuckovic@js-soft.rs>
2023-12-14 14:59:59 +0100

    test: should return love

---
+++ vvuckovi/src/KataTests/Tennis/score.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable prettier/prettier */
+export class Score {
+    ConvertPlayerPoints(point: number) : string {
+        switch(point) {
+            default: return 'love';
+        }
+    }
+}
--- vvuckovi/src/KataTests/Tennis/tennis.spec.ts
+++ vvuckovi/src/KataTests/Tennis/tennis.spec.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
 import { Player } from './player';
+import { Score } from './score';
 
 describe('Kata TDD Tennis Game: Player', () => {
     it('Should_Return_IncrementedPlayerScore', () => {
@@ -8,10 +9,10 @@ describe('Kata TDD Tennis Game: Player', () => {
         const player = new Player();
 
         // Act
-        player.incrementPlayerScore();
+        player.incrementPlayerPoints();
 
         // Assert
-        expect(player.getPlayerScore()).toEqual(1);
+        expect(player.getPlayerPoints()).toEqual(1);
     })
 });
 
