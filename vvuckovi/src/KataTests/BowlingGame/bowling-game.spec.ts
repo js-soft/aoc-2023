2d73ea165dc41e7e01a1c15a2d68218d7d048289
Vladimir Vuckovic <vladimir.vuckovic@js-soft.rs>
2023-12-14 11:10:20 +0100

    bowling game complete

--- vvuckovi/src/KataTests/BowlingGame/bowling-game.spec.ts
+++ vvuckovi/src/KataTests/BowlingGame/bowling-game.spec.ts
@@ -4,8 +4,11 @@ import { Game } from './bowling-game'
 
 describe('Kata TDD Bowling Game', () => {
 
-    const game = new Game();
+    let game: Game; 
 
+    function setUp() : void {
+        game = new Game();
+    }
     
     function rollMany(n: number, pins: number) : void {
         for(let i = 0; i < n; i++){
@@ -13,8 +16,19 @@ describe('Kata TDD Bowling Game', () => {
         }
     }
 
+    
+    function rollSpare(game: Game) {
+        game.roll(5);
+        game.roll(5);
+    }
+
+    function rollStrike() {
+        game.roll(10);
+    }
+
     it('Should_Return_GutterGame', () => {
         // Arrange
+        setUp();
 
         // Act
         rollMany(20, 0);
@@ -25,6 +39,7 @@ describe('Kata TDD Bowling Game', () => {
 
     it('Should_Return_AllOnes', () => {
         // Arrange
+        setUp();
 
         // Act
         rollMany(20, 1);
@@ -35,14 +50,40 @@ describe('Kata TDD Bowling Game', () => {
 
     it('Should_Return_OneSpare', () => {
         // Arrange
-        game.roll(5);
-        game.roll(5);
-        game.roll(3);
-        
+        setUp();
+
         // Act
+        rollSpare(game);
+        game.roll(3);
         rollMany(17,0);
 
-        //Assert
+        // Assert
         expect(game.score()).toEqual(16);
     });
+
+    it('Should_Return_OneStrike', () => {
+        // Arrange
+        setUp();
+
+        // Act
+        rollStrike();
+        game.roll(3);
+        game.roll(4);
+        rollMany(16,0);
+
+        // Assert
+        expect(game.score()).toEqual(24);
+    });
+
+    it('Should_Return_PerfectGame', () => {
+        // Arrange
+        setUp();
+
+        // Act
+        rollMany(12,10);
+
+        // Assert
+        expect(game.score()).toEqual(300); 
+    });
 });
+
--- vvuckovi/src/KataTests/BowlingGame/bowling-game.ts
+++ vvuckovi/src/KataTests/BowlingGame/bowling-game.ts
@@ -2,8 +2,12 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
 export class Game {
-    private rolls!: Array<number>;
+    private rolls: number[];
     private currentRoll = 0;
+
+    constructor() {
+        this.rolls = [];
+    }
 
     public roll(pins: number) : void {
         this.rolls[this.currentRoll++] = pins;
@@ -11,11 +15,43 @@ export class Game {
 
     public score() : number {
         let score = 0;
+        let frameIndex = 0;
 
-        for(let i = 0; this.rolls.length; i++){
-            score += this.rolls[i];
+        for(let frame = 0; frame < 10; frame++){
+            if(this.isStrike(frameIndex)){
+                score += 10 + this.strikeBonus(frameIndex);
+                frameIndex++;
+            }
+            else if(this.isSpare(frameIndex)){
+                score += 10 + this.spareBonus(frameIndex);
+                frameIndex += 2;
+            }else {
+                score += this.sumOfBallsInFrame(frameIndex);
+                frameIndex += 2;
+            }
         }
 
         return score;
     }
+
+    private isStrike(frameIndex: number) {
+        return this.rolls[frameIndex] == 10;
+    }
+
+    private strikeBonus(frameIndex: number) {
+       return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2]
+    }
+
+    private spareBonus(frameIndex: number) {
+        return this.rolls[frameIndex + 2];
+    }
+
+    private sumOfBallsInFrame(frameIndex: number) {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+    }
+
+    private isSpare(frameIndex: number) {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10;
+    }
+
 }
