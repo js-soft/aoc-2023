39e1bbf6ed431bbbb9dec7f73cbe986e7223d6be
Vladimir Vuckovic <vladimir.vuckovic@js-soft.rs>
2023-12-14 15:53:15 +0100

    chore: checking for deuce

--- vvuckovi/src/KataTests/Tennis/game.ts
+++ vvuckovi/src/KataTests/Tennis/game.ts
@@ -1,12 +1,19 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 
 import { Player } from "./player";
+import { Converters } from "./converters";
 
 /* eslint-disable prettier/prettier */
 export class Game {
     
-    PlayGameOfTennis(playerOne: Player, playerTwo: Player) : string {
-        return "15-love";
+    PlayGameOfTennis(player1: Player, player2: Player) : string {
+        const convert = new Converters();
+        
+        if(player1.getPlayerPoints() === player2.getPlayerPoints() && player1.getPlayerPoints() >= 3) {
+            return "deuce";
+        }
+
+        return convert.ConvertPlayerPoints(player1.getPlayerPoints()) + "-" + convert.ConvertPlayerPoints(player2.getPlayerPoints());
     }
-    
+
 }
