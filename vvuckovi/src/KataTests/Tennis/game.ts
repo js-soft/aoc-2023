8eb703783632de16440df0e08338d1ce7145b462
Vladimir Vuckovic <vladimir.vuckovic@js-soft.rs>
2023-12-14 15:23:40 +0100

    feat: implement basic game

---
+++ vvuckovi/src/KataTests/Tennis/game.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+import { Player } from "./player";
+
+/* eslint-disable prettier/prettier */
+export class Game {
+    
+    PlayGameOfTennis(playerOne: Player, playerTwo: Player) : string {
+        return "15-love";
+    }
+    
+}
